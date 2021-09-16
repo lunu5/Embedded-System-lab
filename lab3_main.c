204f0e1e093e6bff45807d40138c6449ed0a584e
lunu5 <80402166+lunu5@users.noreply.github.com>
2021-09-16 21:22:55 +0700

    Update lab3_main.c

--- lab3_main.c
+++ lab3_main.c
@@ -15,10 +15,8 @@ void task(void *pv)
         printf("%s begins at %f\n", (char *)pv, (double)((double)begin / CLOCKS_PER_SEC));
         printf("configUSE_PREEMPTION: %d\n", configUSE_PREEMPTION);
         printf("configUSE_TIME_SLICING: %d\n", configUSE_TIME_SLICING);
-        while ((double)(clock() - begin) < duration)
-        {
-            printf("%s ends at %f from %f\n", (char *)pv, (double)((double)clock() / CLOCKS_PER_SEC), (double)((double)begin / CLOCKS_PER_SEC));
-        }
+        while ((double)(clock() - begin) < duration){}
+        printf("%s ends at %f from %f\n", (char *)pv, (double)((double)clock() / CLOCKS_PER_SEC), (double)((double)begin / CLOCKS_PER_SEC));
         vTaskDelay(2000 / portTICK_RATE_MS);
     }
 }
@@ -29,8 +27,8 @@ static const char *pcTextForTask3 = " Task 3\r\n";
 void app_main(void)
 {
     xTaskCreatePinnedToCore(&task, "Task 1", 10000, (void *)pcTextForTask1, 1, NULL, 1);
-    vTaskDelay(500 / portTICK_RATE_MS);
+    vTaskDelay(400 / portTICK_RATE_MS);
     xTaskCreatePinnedToCore(&task, "Task 2", 10000, (void *)pcTextForTask2, 1, NULL, 1);
-    vTaskDelay(4000 / portTICK_RATE_MS);
+    vTaskDelay(4200 / portTICK_RATE_MS);
     xTaskCreatePinnedToCore(&task, "Task 3", 5000, (void *)pcTextForTask3, 2, NULL, 1);
 }
