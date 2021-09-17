e51ea3108da1022d7ba601234415b84a03f4448d
lunu5 <80402166+lunu5@users.noreply.github.com>
2021-09-17 14:31:10 +0700

    Update lab3_main.c

--- lab3_main.c
+++ lab3_main.c
@@ -5,30 +5,48 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
-#define duration 1000
 
-void task(void *pv)
+#define idleTaskDuration 5000
+#define preemptTaskDuration 1500
+#define delay 1000
+
+void vIdleTask(void *pv)
 {
     for (;;)
     {
         clock_t begin = clock();
         printf("%s begins at %f\n", (char *)pv, (double)((double)begin / CLOCKS_PER_SEC));
-        printf("configUSE_PREEMPTION: %d\n", configUSE_PREEMPTION);
-        printf("configUSE_TIME_SLICING: %d\n", configUSE_TIME_SLICING);
-        while ((double)(clock() - begin) < duration){}
+        while ((double)(clock() - begin) < idleTaskDuration) {}
         printf("%s ends at %f from %f\n", (char *)pv, (double)((double)clock() / CLOCKS_PER_SEC), (double)((double)begin / CLOCKS_PER_SEC));
-        vTaskDelay(2000 / portTICK_RATE_MS);
+        vTaskDelay(delay / portTICK_RATE_MS);
     }
+    vTaskDelete(NULL);
 }
-static const char *pcTextForTask1 = " Task 1\r\n";
-static const char *pcTextForTask2 = " Task 2\r\n";
+
+void vPreemptTask(void *pv)
+{
+    for (;;)
+    {
+        clock_t begin = clock();
+        printf("%s begins at %f\n", (char *)pv, (double)((double)begin / CLOCKS_PER_SEC));
+        while ((double)(clock() - begin) < preemptTaskDuration) {}
+        printf("%s ends at %f from %f\n", (char *)pv, (double)((double)clock() / CLOCKS_PER_SEC), (double)((double)begin / CLOCKS_PER_SEC));
+        vTaskDelay(delay / portTICK_RATE_MS);
+    }
+    vTaskDelete(NULL);
+}
+
+static const char *pcTextForTask1 = " Idle task 1\r\n";
+static const char *pcTextForTask2 = " Idle task 2\r\n";
 static const char *pcTextForTask3 = " Task 3\r\n";
 
 void app_main(void)
 {
-    xTaskCreatePinnedToCore(&task, "Task 1", 10000, (void *)pcTextForTask1, 1, NULL, 1);
-    vTaskDelay(400 / portTICK_RATE_MS);
-    xTaskCreatePinnedToCore(&task, "Task 2", 10000, (void *)pcTextForTask2, 1, NULL, 1);
-    vTaskDelay(4200 / portTICK_RATE_MS);
-    xTaskCreatePinnedToCore(&task, "Task 3", 5000, (void *)pcTextForTask3, 2, NULL, 1);
+    printf("configUSE_PREEMPTION: %d\n", configUSE_PREEMPTION);
+    printf("configUSE_TIME_SLICING: %d\n", configUSE_TIME_SLICING);
+    xTaskCreatePinnedToCore(&vIdleTask, "Task 1", 4096, (void *)pcTextForTask1, 1, NULL, 1);
+    vTaskDelay(200 / portTICK_RATE_MS);
+    xTaskCreatePinnedToCore(&vIdleTask, "Task 2", 4096, (void *)pcTextForTask2, 1, NULL, 1);
+    vTaskDelay(1200 / portTICK_RATE_MS);
+    xTaskCreatePinnedToCore(&vPreemptTask, "Task 3", 4096, (void *)pcTextForTask3, 2, NULL, 1);
 }
